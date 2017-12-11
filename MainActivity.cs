8a6f04733e6a56bf8be74d28d3171e699e46c0d9
Josh Cross <crossjoshua6@gmail.com>
2017-12-11 17:54:25 +0000

    created a simple class that will contain all the JSON lists.

---
+++ MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace CarCosts
+{
+    [Activity(Label = "CarCosts", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            //getting the title from the application.
+            TextView title = FindViewById<TextView>(Resource.Id.Test);
+            //setting the title.
+            try
+            {
+                title.Text = "Testing";
+            } catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
